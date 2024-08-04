215966ea43f10f629d49212e043bca72c01155c1
Shivansh Baveja <shivansh.baveja@gmail.com>
2024-08-03 17:59:22 -0700

    integrated with gym.Env interface

--- maze_env.py
+++ maze_env.py
@@ -1,20 +1,31 @@
 import os
+import gym
+from gym import spaces
 import imageio
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import colors as mcolors
 
 
-class MazeGenerator:
+class MazeEnv(gym.Env):
     def __init__(self, maze_height, maze_width, animation=False, endpoints=True):        
-        # initialize action space
-        self.action_space = np.array([[0, 1],
+        # initialize directions
+        self.directions = np.array([[0, 1],
                              [0, -1],
                              [1, 0],
                              [-1, 0]])
         
-        # create GIF or not
+        # setting color bounds
+        bounds = [0, 1, 2, 3, 4, 5]
+        self.cmap = mcolors.ListedColormap(['white', 'black', 'red', 'green', 'orange'])
+        self.norm = mcolors.BoundaryNorm(bounds, self.cmap.N)
+        
+        # create animated GIFs or not
         self.animation = animation
+
+        # initialize animation storage
+        self.intermediates = {'generation': [],
+                              'training': []}
 
         # set endpoint color flag
         self.endpoints = endpoints
@@ -22,18 +33,86 @@ class MazeGenerator:
         # save width and height attributes        
         self.width, self.height = maze_width, maze_height
 
-        # set endpoint position
+        # calculate endpoint position
         self.finish = self.get_finish()
 
         # default starting position in top left corner
         self.start_position = np.array([0,0])
 
+        # start agent in starting position
+        self.agent_position = self.start_position
+
+        # four possible actions: left right up down
+        self.action_space = spaces.Discrete(4)
+        self.observation_space = spaces.Box(low=0, high=max(maze_height, maze_width), shape=(2,), dtype=np.uint8)
+
         # generate maze using DFS from start position
         self.generate_maze(self.start_position)
 
+
+    def reset(self , seed=None, options=None):
+        # reset start, end, and current agent position
+        self.maze[tuple(self.agent_position)] = 0
+        self.maze[tuple(self.start_position)] = 3
+        self.maze[tuple(self.finish)] = 2
+
+        # reset agent position to beginning
+        self.agent_position = self.start_position
+
+        # set starting position to agent value for visualizations
+        self.maze[tuple(self.start_position)] = 4
+
+        # reset animations per episode
+        self.intermediates['training'] = []
+        
+        observation = tuple(self.agent_position)
+        info = {}
+
+        return observation, info
+    
+    def is_valid_position(self, position):
+        if (0 <= position[0] < self.height and 0 <= position[1] < self.width and self.maze[tuple(position)] != 1):
+            return True
+        return False
+    
+    def step(self, action):
+        # execute the action
+        new_position = self.agent_position + self.directions[action]
+
+        if self.is_valid_position(new_position):
+            # clear previous position     
+            self.maze[tuple(self.agent_position)] = 0 
+
+            # move agent to new position 
+            self.agent_position = new_position
+            self.maze[tuple(self.agent_position)] = 4
+
+            # add to animations
+            self.intermediates['training'].append(np.array(self.maze))
+
+        # check if agent is finished
+        done = np.all(self.agent_position == self.finish)
+
+        # calculate reward
+        if done:
+            reward = 10.00
+        # penalize trying to go through walls
+        elif self.is_valid_position(new_position) and self.maze[tuple(new_position)] == 1:
+            reward = -1.00
+        else:
+            reward = -0.10
+
+        observation = tuple(self.agent_position)
+        info = {}
+
+        return observation, reward, done, False, info
+    
+    def render(self):
+        self.save_frame(self.maze, mode='render')
+
     def dfs(self, position):
         # randomize step order
-        steps = np.random.permutation(self.action_space)
+        steps = np.random.permutation(self.directions)
 
         # take each step, using DFS
         for step in steps:
@@ -53,14 +132,14 @@ class MazeGenerator:
                     
                     # if animate, add copy to intermediate maze array for later animation
                     if self.animation:
-                        self.intermediates.append(np.array(self.maze))
+                        self.intermediates['generation'].append(np.array(self.maze))
 
                     # continue DFS
                     self.dfs(next_position)
 
     def generate_maze(self, position):
         # save intermediate mazes for animation
-        self.intermediates = []
+        self.intermediates['generation'] = []
 
         # save frame filenames
         self.frames = []
@@ -79,28 +158,29 @@ class MazeGenerator:
         # run dfs from position
         self.dfs(position)
     
-    def animate(self):
+    def animate(self, mode='generation'):
+        # reset frames for animating
+        self.frames = []
+
         if not self.animation:
             raise RuntimeError("Animation is not enabled.")
         
         # save all frames as images
-        for frame in self.intermediates:
+        for frame in self.intermediates[mode]:
             self.save_frame(frame)
 
         # make and save gif
-        self.create_gif_and_cleanup()
+        self.create_gif_and_cleanup(mode=mode)
 
-    def save_frame(self, array):
+    def save_frame(self, array, mode='animate'):
         # filename for frame
-        filename = f'frame_{len(self.frames)}.png'
-
-        # setting color bounds
-        bounds = [0, 1, 2, 3, 4]
-        cmap = mcolors.ListedColormap(['white', 'black', 'red', 'green'])
-        norm = mcolors.BoundaryNorm(bounds, cmap.N)
+        if mode == 'animate':
+            filename = f'frame_{len(self.frames)}.png'
+        elif mode == 'render':
+            filename = 'render.png'
 
         # plotting settings
-        plt.imshow(array, cmap=cmap, norm=norm)
+        plt.imshow(array, cmap=self.cmap, norm=self.norm)
         plt.axis('off')
 
         # save and close image
@@ -110,14 +190,14 @@ class MazeGenerator:
         # append filename
         self.frames.append(filename)
         
-    def create_gif_and_cleanup(self, duration=0.5):
+    def create_gif_and_cleanup(self, duration=0.5, mode='generation'):
         # read in all simages
         images = []
         for filename in self.frames:
             images.append(imageio.imread(filename))
             
         # save into gif
-        imageio.mimsave('maze_generation.gif', images, duration=duration)
+        imageio.mimsave(f'{mode}.gif', images, duration=duration)
         
         # delete the frames
         for filename in self.frames:
